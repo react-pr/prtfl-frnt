6056689522f46334932f113d98f421a08e09c0e4
polinom <kutsenko.pn@ucu.edu.ua>
2024-01-21 14:00:41 +0200

    Added new servise to display get request | commit

--- src/components/screens/Contact/Contact.tsx
+++ src/components/screens/Contact/Contact.tsx
@@ -1,16 +1,32 @@
 import { Layout } from '@/components/Layout/Layout'
 import { IFormData } from '@/interfaces/Interfaces'
 import { SendMainService } from '@/services/sendMail/SendMailService'
+import { useEffect, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 export const Contact = () => {
+	const [response, setResponse] = useState<string>('')
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm<IFormData>()
+		reset,
+	} = useForm<IFormData>({
+		mode: 'onChange',
+	})
+	useEffect(() => {
+		const getResult = async () => {
+			const result = await SendMainService.getRequest()
+			setResponse(result)
+			console.log(response)
+		}
+		getResult()
+	}, [])
 	const formSubmit: SubmitHandler<IFormData> = async data => {
 		await SendMainService.postData(data)
+		const message = await SendMainService.postData(data)
+		console.log(message)
+		reset()
 	}
 	return (
 		<Layout>
--- src/services/sendMail/SendMailService.ts
+++ src/services/sendMail/SendMailService.ts
@@ -7,6 +7,12 @@ export const SendMainService = {
 			`${import.meta.env.VITE_BACK_END_URL}sending/`,
 			data
 		)
-		return request
+		return request.data
+	},
+	async getRequest() {
+		const response = await axios.get<string>(
+			`${import.meta.env.VITE_BACK_END_URL}test-get/`
+		)
+		return response.data
 	},
 }
